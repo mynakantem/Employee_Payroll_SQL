6b7f2700f23f9f7dfb017ae49c0773ef7f56e6c1
mynakantem <mr6936@srmist.edu.in>
2025-05-17 11:00:00 +0530

    Added UC 2 - REST Controller

---
+++ Employee_Payroll_SQL-UC_2_REST_Controller/src/main/java/com/example/employeeapp/EmployeePayrollSqlUc1ProjectSetupAndRestControllerApplication.java
@@ -0,0 +1,14 @@
+package com.example.employeeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmployeePayrollSqlUc1ProjectSetupAndRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollSqlUc1ProjectSetupAndRestControllerApplication.class, args);
+	}
+
+}
