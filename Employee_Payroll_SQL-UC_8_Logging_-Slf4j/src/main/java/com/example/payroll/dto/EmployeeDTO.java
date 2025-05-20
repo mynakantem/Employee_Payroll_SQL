f24fb9162e911b5ca56f529c5444ada02f876297
mynakantem <mr6936@srmist.edu.in>
2025-05-20 18:00:00 +0530

    Added UC 8 - Logging with Slf4j

---
+++ Employee_Payroll_SQL-UC_8_Logging_-Slf4j/src/main/java/com/example/payroll/dto/EmployeeDTO.java
@@ -0,0 +1,13 @@
+package com.example.payroll.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class EmployeeDTO {
+    private String name;
+    private long salary;
+}
